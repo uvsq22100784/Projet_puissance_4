6f90454e7f3d3753bbbef2a5b3ee45c0d2c061f2
Laura LECLERC <laura.leclerc@ens.uvsq.fr>
2022-04-27 11:18:48 +0200

    grille

--- projet_puissance_4.py
+++ projet_puissance_4.py
@@ -1,6 +1,7 @@
-import tkinter as tk
-import random as rd
+from tkinter import *
+import random
 
+jeu=Tk()
 # CONSTANTES
 TITRE = "PUISSANCE 4"
 # Nombre de lignes de l’aire de jeu
@@ -16,35 +17,82 @@ VIDE = 0 ; ROUGE = 1 ; JAUNE = 2
 # Lettre associée
 SYMBOLE_COULEUR = (" ","*","o")
 
-# Définition d’un joueur
-class Joueur :
-    def __init__ (outSelf, inCouleur, inNom="") :
-        outSelf.couleur = int(inCouleur) # En fait, soit ROUGE, soit JAUNE
-        outSelf.nom = str(inNom) # Le nom du joueur
-        outSelf.ordinateur = bool(inNom=="") # Est-ce l’ordinateur ?
-        if inNom == "" : outSelf.nom = "Ordinateur"
-        outSelf.nb_victoires = int(0) # Nombre de victoires
-# Définition des composants du jeu
-class Jeu :
-    def __init__(outSelf,inNomJoueur1,inNomJoueur2="") :
-#1.grille de jeu avec une bordure supplémentaire sur les 4 bords
-        outSelf.grille = [] # liste vide => on va lui rajouter les lignes
-for ligne in range(NB_LIGNES+2) : # 2 lignes de plus pour les bords
-    def symbole_case(inCouleur) : # str
-        return SYMBOLE_COULEUR[inCouleur]
-def afficher_numeros_colonnes() :
-    j = int() # index pour parcourir les colonnes de la grille
-print(" ",end=" ") # espace pour le bord droit
-for j in range(1,NB_COLONNES+1) : print(j,end=" ")
-print () # pour aller à la Ligne
-def afficher_grille(inJeu) :
-    i = int() ; j = int() # index pour parcourir les cases de la grille
-#1.afficher le titre
-print() # sauter une ligne
-print(" ",TITRE) # ajout de 2 espaces pour centrer le titre sur la grille
-#2.afficher le bord supérieur
-afficher_numeros_colonnes()
-#3.afficher la grille avec les bords gauche et droit
-for i in range(NB_LIGNES,0,-1) : # on affiche d’abord la ligne supérieure
-#3.1.afficher le numéro de ligne à gauche
-    print (i,end=" ")
+def trace_ligne_ver(x):
+    x1=x
+    y1=100
+    x2=x
+    y2=700
+    Zone.create_line(x1,y1,x2,y2,width=1,fill="black")
+
+def trace_ligne_hor(x):
+    x1=100
+    y1=x
+    x2=800
+    y2=x
+    Zone.create_line(x1,y1,x2,y2,width=1,fill="black")
+
+Zone=Canvas(jeu,width=1000,height=900,bg="white")
+Zone.place(x=0,y=0)
+x=0
+
+while x!=900:
+    trace_ligne_ver(x)
+    x=x+100
+
+x=0
+while x!=800:
+    trace_ligne_hor(x)
+    x=x+100
+
+Joueur=random.randint(1,2)
+total=0
+def Attendre_clic(event):
+    global JEU
+    global Joueur
+    Nbre_jetons=0
+    global total
+    xClic,yClic = event.x,event.y
+    if 100<xClic<800: #Vérifier que le clic est dans le tableau
+        colonne=Recherche_colonne(xClic)
+        ligne=Recherche_ligne(colonne)
+
+
+        if Joueur==1: #tour joueur rouge
+            Zone.create_oval(colonne*100-20+150,ligne*100+170,colonne*100+20+150,ligne*100+130, fill='red') #Placement du jeton au milieu de la case
+            Joueur=2
+            print("C'est au tour du joueur 2")
+            JEU[ligne][colonne]=1      #Définir que cette case est rouge dans le tableau
+            total=total+1
+
+
+        elif Joueur==2: #Tour du joueur jaune
+            Zone.create_oval(colonne*100-20+150,ligne*100+170,colonne*100+20+150,ligne*100+130, fill='yellow')
+            Joueur=1
+            print("C'est au tour du joueur 1")
+            JEU[ligne][colonne]=2
+            total=total+1
+
+
+
+    if total==42: #Vérification de tout les jetons placés pour l'égalité
+        print("égalité")
+jeu.bind("<Button-1>", Attendre_clic)
+
+def Recherche_colonne(xClic):
+    colonne=(xClic)//100-1 #Définir dans quelle colonne est le clic
+    return colonne
+
+def Recherche_ligne(colonne):
+    ligne1=5
+    for loop in range (6):        #Vérifier si des jetons sont déjà présent dans la colonne pour choisir la ligne
+        if JEU[ligne1][colonne]==0:
+            ligne=ligne1
+        else:
+            ligne1=ligne1-1
+    return ligne
+
+
+
+chaine = Label(jeu)
+chaine.pack()
+jeu.mainloop()
